0705a50d29f9c924f5636fdd9c8e9efc9e08c9fa
Alvaro Linares <alvarolinares@gmail.com>
2020-06-19 10:49:56 -0300

    WIP envío de mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,6 +13,7 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
+            var destination = $("select#destination").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -26,7 +27,8 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+                    destination: destination
                 },
                 cache: false,
                 success: function() {
@@ -35,7 +37,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Su mensaje fue enviado. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -47,7 +49,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>" + firstName + " te pedimos disculpas, el mensaje no se pudo enviar. Intentá nuevamente más tarde por favor!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
