0a522f2a7ccb4500b43a8626577f4b210c2bdd67
Alvaro Linares <alvarolinares@gmail.com>
2020-06-19 11:40:14 -0300

    archivos de contacto

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,10 +1,23 @@
 <?php
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require '../vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
-   !filter_var($_POST['destination'],FILTER_VALIDATE_EMAIL)) ||
+   !filter_var($_POST['destination'],FILTER_VALIDATE_EMAIL) ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "Argument missing!";
@@ -18,12 +31,40 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = strip_tags(htmlspecialchars($_POST['destination']));
 
 
-print_r(array($name,$email_address,$phone,$message,$destination))
 // Create the email and send the message
 
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$email_subject = "Contacto de vascotour.com.ar: $name";
+$email_body = "Información del contacto:\n\nNombre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
+
+//print_r(array($to,$email_subject,$email_body,$headers));
+
+
+try {
+   //Server settings
+   $mail->isSMTP();                                            // Send using SMTP
+   $mail->Host       = 'dtcwin080.ferozo.com';                    // Set the SMTP server to send through
+   $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+   $mail->Username   = 'no-reply@vascotour.net';                     // SMTP username
+   $mail->Password   = 'a1mZkgmdCr';                               // SMTP password
+   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+   $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+   //Recipients
+   $mail->setFrom('no-reply@vascotour.net', $name);
+   $mail->addReplyTo($email_address, $name);
+   $mail->addAddress($to);     // Add a recipient
+
+   // Content
+   $mail->Subject = $email_subject;
+   $mail->Body    = $email_body;
+
+   $mail->send();
+   echo 'Mensaje enviado';
+} catch (Exception $e) {
+   echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+
 // mail($to,$email_subject,$email_body,$headers);
 return true;         
